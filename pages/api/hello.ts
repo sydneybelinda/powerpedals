e47a86bba5c81b1fe310bd95838586339b389a32
Belinda Launer <launerb@gmail.com>
2023-05-04 10:35:43 +1000

    /

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+const models = require('../../models/index');
 
 type Data = {
   name: string
@@ -9,5 +10,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // console.log(db.sequelize)
+  console.log(models.users)
   res.status(200).json({ name: 'John Doe' })
 }
